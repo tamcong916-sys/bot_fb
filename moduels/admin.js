862aadc3313ea9a37d38e30e09fe88b9edff05c9
tamcong916-sys <tamcong916@gmail.com>
2026-05-26 19:03:56 +0700

    Update admin.js

--- moduels/admin.js
+++ moduels/admin.js
@@ -1,1 +1,5 @@
-
+module.exports = [
+    { name: "kick", async execute(api, event, args) { let id = Object.keys(event.mentions)[0] || args[0]; if(!id) return; api.removeUserFromGroup(id, event.threadID); } },
+    { name: "sendmsg", async execute(api, event, args) { return api.sendMessage(`📢 Thông báo từ Admin: ${args.join(" ")}`, event.threadID); } },
+    { name: "restart", async execute(api, event) { await api.sendMessage("🔄 Hệ thống đang tiến hành khởi động lại...", event.threadID); process.exit(1); } }
+];
