33236ab2606d304de5d66e4709bafdfa5d00b02f
tamcong916-sys <tamcong916@gmail.com>
2026-05-26 19:13:35 +0700

    modules directory

--- moduels/giaitri.js
+++
@@ -1,6 +0,0 @@
-const axios = require('axios');
-module.exports = [
-    { name: "anime", async execute(api, event) { const res = await axios.get("https://api.waifu.pics/sfw/waifu"); const img = await axios.get(res.data.url, {responseType: "stream"}); return api.sendMessage({body: "🌸 Ảnh Waifu Anime ngẫu nhiên:", attachment: img.data}, event.threadID); } },
-    { name: "meme", async execute(api, event) { const img = await axios.get("https://picsum.photos/500/500", {responseType: "stream"}); return api.sendMessage({body: "🤣 Meme giải trí cười ỉa:", attachment: img.data}, event.threadID); } },
-    { name: "joker", async execute(api, event) { return api.sendMessage("🤡 Trò đùa hôm nay: Tại sao lập trình viên thích bóng tối? Vì ánh sáng làm lộ ra quá nhiều BUG!", event.threadID); } }
-];
--- moduels/tinhtien.js
+++
@@ -1,5 +0,0 @@
-module.exports = [
-    { name: "daily", async execute(api, event) { return api.sendMessage("🎁 Bạn đã nhận $200 điểm danh hằng ngày!", event.threadID); } },
-    { name: "work", async execute(api, event) { const jobs = ["sửa xe", "bán cá", "viết code bot"]; return api.sendMessage(`💼 Bạn làm nghề [${jobs[Math.floor(Math.random()*3)]}] và kiếm được $${Math.floor(Math.random()*100)+50}!`, event.threadID); } },
-    { name: "checkmoney", async execute(api, event) { return api.sendMessage("💰 Số dư tài khoản ảo của bạn hiện có: $15,000", event.threadID); } }
-];
